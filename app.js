773413ae8e0ca8f487870cafa31267ee61728969
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-06-11 18:19:27 +0500

    JS FILE

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // function changeImgRed() {
-//     var image = document.getElementsByClassName('myImg');
+//     var image = document.getElementsByClassName('c1');
 //     if (image.src !="images/red.png") {
 //         image.src = "images/red.png";
 //     }
 // }
 // function changeImgYellow() {
-//     var image = document.getElementsByClassName('myImg');
+//     var image = document.getElementsByClassName('c2');
 //     if (image.src !="images/yellow.png") {
 //         image.src = "images/yellow.png";
 //     }
 // }
 // function changeImgGreen() {
-//     var image = document.getElementsByClassName('myImg');
+//     var image = document.getElementsByClassName('c3');
 //     if (image.src !="images/green.png") {
 //         image.src = "images/green.png";
 //     }
